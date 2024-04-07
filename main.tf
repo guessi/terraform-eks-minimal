007b48e2c41ea96f7046f53807c38a94ebdd99fe
guessi <guessi@gmail.com>
2024-04-07 14:30:15 +0800

    Support for creating AL2023 node groups

--- main.tf
+++ main.tf
@@ -3,8 +3,9 @@ locals {
   region          = "us-east-1"
   cluster_version = "1.29"
 
-  ami_type  = "AL2_x86_64"
-  disk_size = 30
+  ami_type_AL2    = "AL2_x86_64"
+  ami_type_AL2023 = "AL2023_x86_64_STANDARD"
+  disk_size       = 30
 
   instance_type = [
     "t3a.small",
@@ -80,7 +81,6 @@ module "eks" {
   cluster_service_ipv4_cidr = local.cluster_service_ipv4_cidr
 
   eks_managed_node_group_defaults = {
-    ami_type       = local.ami_type
     disk_size      = local.disk_size
     instance_types = local.instance_type
 
@@ -97,6 +97,7 @@ module "eks" {
   // - https://github.com/bryantbiggs/eks-desired-size-hack
   eks_managed_node_groups = {
     mng1 = {
+      ami_type        = local.ami_type_AL2
       use_name_prefix = false
 
       min_size     = local.min_size
@@ -104,10 +105,12 @@ module "eks" {
       desired_size = local.desired_size
 
       capacity_type = "SPOT"
+    }
 
-      update_config = {
-        max_unavailable_percentage = 25 # or set `max_unavailable`
-      }
+    al2023_nodeadm = {
+      ami_type                       = local.ami_type_AL2023
+      platform                       = "al2023"
+      use_latest_ami_release_version = true
     }
   }
 
