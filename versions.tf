77f689b8b8b137065b3866d6b6db50e4d0e797ed
guessi <guessi@gmail.com>
2024-10-05 13:16:39 +0800

    Bump dependencies

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.68"
+      version = "~> 5.70"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
