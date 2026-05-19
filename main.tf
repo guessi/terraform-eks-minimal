9f6e8f06ce7a75971ce8763528d3099831f3b0d3
guessi <guessi@gmail.com>
2026-05-19 11:00:17 +0800

    Fix terraform-aws-eks v21.0.0 breaking changes
    - https://github.com/terraform-aws-modules/terraform-aws-eks/releases/tag/v21.0.0

--- main.tf
+++ main.tf
@@ -35,18 +35,18 @@ locals {
 module "eks" {
   # https://registry.terraform.io/modules/terraform-aws-modules/eks/aws/latest
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.1.3"
+  version = "~> 21.20.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = local.cluster_version
+  name               = local.cluster_name
+  kubernetes_version = local.cluster_version
 
-  cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = true
+  endpoint_private_access = true
+  endpoint_public_access  = true
 
   authentication_mode                      = "API_AND_CONFIG_MAP"
   enable_cluster_creator_admin_permissions = true
 
-  cluster_addons = {
+  addons = {
     kube-proxy = {
       most_recent       = true
       resolve_conflicts = "OVERWRITE"
@@ -86,57 +86,46 @@ module "eks" {
   // - terraform-aws-modules/eks/aws >= 20.21.0
   //   - https://github.com/terraform-aws-modules/terraform-aws-eks/releases/tag/v20.21.0
   //   - https://github.com/terraform-aws-modules/terraform-aws-eks/pull/3112
-  cluster_upgrade_policy = {
+  upgrade_policy = {
     support_type = local.cluster_upgrade_policy
   }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
-  cluster_ip_family         = local.cluster_ip_family
-  cluster_service_ipv4_cidr = local.cluster_service_ipv4_cidr
-
-  eks_managed_node_group_defaults = {
-    ami_type       = local.ami_type_AL2023
-    instance_types = local.instance_types
-
-    use_name_prefix = false
-
-    min_size = local.min_size
-    max_size = local.max_size
-    # This value is ignored after the initial creation
-    # https://github.com/bryantbiggs/eks-desired-size-hack
-    desired_size = local.desired_size
-
-    use_latest_ami_release_version = true
-
-    capacity_type = "SPOT"
-
-    block_device_mappings = {
-      xvda = {
-        device_name = "/dev/xvda"
-        ebs = {
-          volume_size           = local.volume_size
-          volume_type           = local.volume_type
-          delete_on_termination = true
-        }
-      }
-    }
-
-    # We are using the IRSA created below for permissions
-    # However, we have to deploy with the policy attached FIRST (when creating a fresh cluster)
-    # and then turn this off after the cluster/node group is created. Without this initial policy,
-    # the VPC CNI fails to assign IPs and nodes cannot join the cluster
-    # See https://github.com/aws/containers-roadmap/issues/1666 for more context
-    iam_role_attach_cni_policy = true
-  }
+  ip_family         = local.cluster_ip_family
+  service_ipv4_cidr = local.cluster_service_ipv4_cidr
 
   // HINT: be aware of the desired_size changes
   // - https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/docs/faq.md#why-are-there-no-changes-when-a-node-groups-desired_size-is-modified
   // - https://github.com/bryantbiggs/eks-desired-size-hack
   eks_managed_node_groups = {
     al2023-mng1 = {
-      ami_type = local.ami_type_AL2023
+      ami_type       = local.ami_type_AL2023
+      instance_types = local.instance_types
+
+      use_name_prefix = false
+
+      min_size     = local.min_size
+      max_size     = local.max_size
+      desired_size = local.desired_size
+
+      use_latest_ami_release_version = true
+
+      capacity_type = "SPOT"
+
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size           = local.volume_size
+            volume_type           = local.volume_type
+            delete_on_termination = true
+          }
+        }
+      }
+
+      iam_role_attach_cni_policy = true
     }
   }
 
@@ -150,7 +139,7 @@ module "eks" {
 module "vpc" {
   # https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 6.0.1"
+  version = "~> 6.6.1"
 
   name = local.cluster_name
   cidr = local.vpc_cidr
